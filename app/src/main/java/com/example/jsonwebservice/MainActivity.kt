a153829bd2af0d80cdb7a6384de1f25b1a15913a
Jimlop04 <jimmy.lopez2017@uteq.edu.ec>
2022-06-24 01:57:05 -0500

    Modificaciones

--- app/src/main/java/com/example/jsonwebservice/MainActivity.kt
+++ app/src/main/java/com/example/jsonwebservice/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
@@ -17,23 +18,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun VerificarUsuario(view: View){
-
-        val intent = Intent(this@MainActivity, MainActivity2::class.java)
 
         val txtUsu = findViewById<EditText>(R.id.txt_usuario);
         val txtPass = findViewById<EditText>(R.id.txt_clave);
+        val boton = findViewById<Button>(R.id.btn_Ingresar);
 
-        val b = Bundle();
-        b.putString("Usuario", txtUsu.getText().toString());
-        b.putString("Clave", txtPass.getText().toString());
 
-        intent.putExtras(b);
-        startActivity(intent);
+            boton.setOnClickListener {
+
+            if (txtUsu.getText().toString().equals("jimmy") &&
+                txtPass.getText().toString().equals("1234"))
+            {
+                val intent = Intent(this@MainActivity, MainActivity2::class.java)
+                val b = Bundle();
+                b.putString("Usuario", txtUsu.getText().toString());
+                b.putString("Clave", txtPass.getText().toString());
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+            else{
+                val text = "Usuario Incorrecto"
+                val duration = Toast.LENGTH_SHORT
+                val toast = Toast.makeText(applicationContext, text, duration)
+                toast.show()
+            }
+        }
 
     }
+
+
 
 }
 
